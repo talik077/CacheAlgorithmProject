2201da4ea3348f40f775c87554ab328814638087
Shahar Shalev <shaharking@gmail.com>
2019-01-29 00:43:15 +0200

    feat: my sumbra

--- src/main/java/com/hit/memory/CacheUnit.java
+++ src/main/java/com/hit/memory/CacheUnit.java
@@ -8,56 +8,71 @@ import java.util.HashMap;
 import com.hit.algorithm.IAlgoCache;
 import com.hit.dao.IDao;
 
+public class CacheUnit<T> {
 
-public class CacheUnit<T>
-{
-	
 	protected IAlgoCache<Long, DataModel<T>> m_IAlgoCache;
-	
-	public CacheUnit(IAlgoCache<Long,DataModel<T>> algo, IDao<Long,DataModel<T>> dao) 
-	{
-		this.m_IAlgoCache = algo;	
+	protected IDao<Long, DataModel<T>> m_Dao;
+
+	public CacheUnit(IAlgoCache<Long, DataModel<T>> algo, IDao<Long, DataModel<T>> dao) {
+		this.m_IAlgoCache = algo;
+		this.m_Dao = dao;
 	}
-	
-	public DataModel<T>[] getDataModels(Long[] ids)
-	{
-		ArrayList<T> list = new ArrayList<T>();
-		for (long id : ids)
-		{
-			T temp = (T)this.m_IAlgoCache.getElement(id);
-		 	list.add(temp);
-		}
-		
-		DataModel<T>[] ts = (DataModel<T>[])new Object[list.size()];
-		ts = list.toArray(ts);
-	    return ts;
-	}
-	
-	public DataModel<T>[] putDataModels(DataModel<T>[] datamodels)
-	{
-		HashMap<Long, DataModel<T>> l = new HashMap<Long, DataModel<T>>();
-		for (DataModel<T> temp : datamodels)
-		{
-			long Id = temp.getDataModelId();
-			l.put(Id, temp);
-			
-			DataModel<T> removedElement = this.m_IAlgoCache.putElement(Id, temp);
-			long removedElementId = removedElement.getDataModelId();
-			if (l.containsKey(removedElementId)) 
-			{
-				l.remove(removedElementId);
+
+	public DataModel<T>[] getDataModels(Long[] ids) {
+		ArrayList<DataModel<T>> list = new ArrayList<DataModel<T>>();
+		for (long id : ids) {
+			DataModel<T> temp = (DataModel<T>) this.m_IAlgoCache.getElement(id);
+			if (temp != null) {
+				list.add(temp);
+			} else {
+				DataModel<T> storedValue = checkStoredDaoCache(id);
+				if (storedValue != null)
+					list.add(storedValue);
 			}
 		}
 		
-		DataModel<T>[] array = (DataModel<T>[]) l.values().toArray();
-		return array;
+		DataModel[] myass = new DataModel[list.size()];
+		list.toArray(myass);
+		return myass;
 	}
-	
-	public void removeDataModels(Long[] ids)
-	{
-		for (Long id : ids)
-		{
-			this.m_IAlgoCache.removeElement(id);
+
+	private DataModel<T> checkStoredDaoCache(Long id) {
+
+		DataModel<T> storedValue = this.m_Dao.find(id);
+		if (storedValue != null) {
+			DataModel<T> removedCachedValue = this.m_IAlgoCache.putElement(id, storedValue);
+			if (removedCachedValue != null) { // algo cache is full
+				this.m_Dao.save(removedCachedValue);
+			}
+			this.m_Dao.delete(storedValue);
+		}
+		return storedValue;
+	}
+
+	public DataModel<T>[] putDataModels(DataModel<T>[] datamodels) {
+		HashMap<Long, DataModel<T>> l = new HashMap<Long, DataModel<T>>();
+		for (DataModel<T> temp : datamodels) {
+			long Id = temp.getDataModelId();
+			l.put(Id, temp);
+
+			DataModel<T> removedElement = this.m_IAlgoCache.putElement(Id, temp);
+			if (removedElement != null) {
+				long removedElementId = removedElement.getDataModelId();
+				this.m_Dao.save(removedElement);
+				// l.remove(removedElementId);
+			}
+		}
+
+		DataModel[] myass = new DataModel[l.size()];
+		l.values().toArray(myass);
+		return myass;
+	}
+
+	public void removeDataModels(Long[] ids) {
+		for (Long id : ids) {
+			if (this.m_IAlgoCache.getElement(id) != null) {
+				this.m_IAlgoCache.removeElement(id);
+			}
 		}
 	}
 }
--- src/main/test/com/hit/memory/CacheUnitTest.java
+++ src/main/test/com/hit/memory/CacheUnitTest.java
@@ -1,5 +1,54 @@
 package com.hit.memory;
 
-public class CacheUnitTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.hit.memory.CacheUnit;
+import com.hit.dm.DataModel;
+import com.hit.algorithm.IAlgoCache;
+import com.hit.algorithm.LRUAlgoCacheImpl;
+import com.hit.dao.DaoFileImpl;
+
+public class CacheUnitTest {
+	private String m_filePath = "output.txt";
+
+	@Test
+	public void TestCacheUnit() {
+		DataModel<String> objFound = null;
+		DataModel<String> one = new DataModel<String>((long) 1, "Avocado");
+		DataModel<String> two = new DataModel<String>((long) 2, "Banana");
+		DataModel<String> three = new DataModel<String>((long) 3, "Apple");
+		DataModel<String> four = new DataModel<String>((long) 4, "Suka");
+
+		IAlgoCache<Long, DataModel<String>> algo = new LRUAlgoCacheImpl<Long, DataModel<String>>(3);
+		DaoFileImpl dao = new DaoFileImpl<String>(this.m_filePath);
+		dao.save(one);
+		dao.save(two);
+		dao.save(three);
+
+		CacheUnit<String> cache = new CacheUnit<String>(algo, dao);
+		Long[] ass = new Long[] { one.getDataModelId() };
+		cache.getDataModels(ass);
+
+		objFound = dao.find(one.getDataModelId());
+		assertEquals(null, objFound);
+
+		cache.getDataModels(new Long[] { two.getDataModelId(), three.getDataModelId() });
+
+		ArrayList<DataModel<String>> tempList = new ArrayList<DataModel<String>>();
+		tempList.add(four);
+		
+		DataModel[] arr = new DataModel[tempList.size()];
+		tempList.toArray(arr);
+
+		cache.putDataModels(arr);
+		objFound = dao.find(one.getDataModelId());
+
+		assertNotEquals(null, objFound);
+	}
 }
