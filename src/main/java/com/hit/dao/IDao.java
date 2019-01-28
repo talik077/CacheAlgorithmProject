1ecc124ccb86b234935daa9cc00760fc14c735b2
Tal <tal.kaptsan@playbuzz.com>
2019-01-28 21:09:42 +0200

    feat(idao): idao interface

--- src/main/java/com/hit/dao/IDao.java
+++ src/main/java/com/hit/dao/IDao.java
@@ -1,5 +1,25 @@
 package com.hit.dao;
 
-public class IDao {
+import java.io.Serializable;
 
+public interface IDao<ID extends Serializable,T>
+{
+	/***
+	 * Saves a given entity.
+	 * @param entity
+	 */
+	public void save(T entity);
+	
+	/***
+	 * Deletes a given entity.
+	 * @param entity
+	 */
+	public void delete(T entity);
+	
+	/***
+	 * Retrieves an entity by its id.
+	 * @param id
+	 * @return
+	 */
+	public T find(ID id);
 }
