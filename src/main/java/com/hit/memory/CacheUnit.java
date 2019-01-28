37d01bc8d0ee6328ec69de8efdc4ababe02c8d3e
Shahar Shalev <shaharking@gmail.com>
2019-01-28 21:57:37 +0200

    Feat: Cache unit

--- src/main/java/com/hit/memory/CacheUnit.java
+++ src/main/java/com/hit/memory/CacheUnit.java
@@ -1,6 +1,10 @@
 package com.hit.memory;
 
 import com.hit.dm.DataModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.hit.algorithm.IAlgoCache;
 import com.hit.dao.IDao;
 
@@ -12,6 +16,47 @@ public class CacheUnit<T>
 	
 	public CacheUnit(IAlgoCache<Long,DataModel<T>> algo, IDao<Long,DataModel<T>> dao) 
 	{
-		this.m_IAlgoCache = algo;
+		this.m_IAlgoCache = algo;	
+	}
+	
+	public DataModel<T>[] getDataModels(Long[] ids)
+	{
+		ArrayList<T> list = new ArrayList<T>();
+		for (long id : ids)
+		{
+			T temp = (T)this.m_IAlgoCache.getElement(id);
+		 	list.add(temp);
+		}
+		
+		DataModel<T>[] ts = (DataModel<T>[])new Object[list.size()];
+		ts = list.toArray(ts);
+	    return ts;
+	}
+	
+	public DataModel<T>[] putDataModels(DataModel<T>[] datamodels)
+	{
+		HashMap<Long, DataModel<T>> l = new HashMap<Long, DataModel<T>>();
+		for (DataModel<T> temp : datamodels)
+		{
+			long Id = temp.getDataModelId();
+			l.put(Id, temp);
+			
+			DataModel<T> removedElement = this.m_IAlgoCache.putElement(Id, temp);
+			long removedElementId = removedElement.getDataModelId();
+			if (l.containsKey(removedElementId)) 
+			{
+				l.remove(removedElementId);
+			}
+		}
+		
+		return (DataModel<T>[]) l.values().toArray();
+	}
+	
+	public void removeDataModels(Long[] ids)
+	{
+		for (Long id : ids)
+		{
+			this.m_IAlgoCache.removeElement(id);
+		}
 	}
 }
