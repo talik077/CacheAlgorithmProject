f662488d6e42e42aef39dcb8dbc0798fb8152702
Shahar Shalev <shaharking@gmail.com>
2019-01-28 21:01:56 +0200

    feat: added some data model

--- src/main/java/com/hit/dm/DataModel.java
+++ src/main/java/com/hit/dm/DataModel.java
@@ -1,5 +1,40 @@
 package com.hit.dm;
 
-public class DataModel {
+import java.io.Serializable;
 
+public class DataModel<T> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected T m_Content;
+	protected Long m_Id;
+	
+	public DataModel(Long id, T content)
+	{
+		this.m_Content = content;
+		this.m_Id = id;
+	}
+	
+	public T getContent()
+	{
+		return this.m_Content;
+	}
+	
+	public Long getDataModelId()
+	{
+		return this.m_Id;
+	}
+	
+	public void setContent(T content)
+	{
+		this.m_Content = content;
+	}
+	
+	public void setDataModelId(Long id)
+	{
+		this.m_Id = id;
+	}
+	
 }
--- src/main/java/com/hit/memory/CacheUnit.java
+++ src/main/java/com/hit/memory/CacheUnit.java
@@ -1,5 +1,17 @@
 package com.hit.memory;
 
-public class CacheUnit {
+import com.hit.dm.DataModel;
+import com.hit.algorithm.IAlgoCache;
+import com.hit.dao.IDao;
 
+
+public class CacheUnit<T>
+{
+	
+	protected IAlgoCache<Long, DataModel<T>> m_IAlgoCache;
+	
+	public CacheUnit(IAlgoCache<Long,DataModel<T>> algo, IDao<Long,DataModel<T>> dao) 
+	{
+		this.m_IAlgoCache = algo;
+	}
 }
