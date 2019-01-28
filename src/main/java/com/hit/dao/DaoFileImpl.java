37deda5febb0ba4e4dd5b26c40b38be69e50edcb
Tal <tal.kaptsan@playbuzz.com>
2019-01-28 21:37:39 +0200

    feat(idaofileimpl): init

--- src/main/java/com/hit/dao/DaoFileImpl.java
+++ src/main/java/com/hit/dao/DaoFileImpl.java
@@ -1,5 +1,39 @@
 package com.hit.dao;
 
-public class DaoFileImpl {
+import com.hit.dm.DataModel;
+
+public class DaoFileImpl<T> implements IDao<Long, DataModel<T>> {
+	
+	public DaoFileImpl(String filepath) {
+		
+	}
+	
+	public DaoFileImpl(String filePath, int capacity)  {
+		
+	}
+	
+	@Override
+	/***
+	 * Saves a given entity.
+	 */
+	public void save(DataModel<T> t) {
+		
+	}
+	
+	@Override	
+	/***
+	 * Deletes a given entity.
+	 */
+	public void delete(DataModel<T> t) {
+		
+	}
+	
+	@Override	
+	/***
+	 * Retrieves an entity by its id.
+	 */
+	public DataModel<T> find(Long id){
+		return null;
+	}
 
 }
